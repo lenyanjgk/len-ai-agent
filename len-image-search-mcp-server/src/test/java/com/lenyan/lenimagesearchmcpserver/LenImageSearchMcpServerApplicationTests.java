0e974865eebd71a3d3d39d8a51968ca4443bf68a
lenyanjgk <128200330+lenyanjgk@users.noreply.github.com>
2025-05-25 19:55:35 +0800

    feat(len-image-search-mcp-server): 实现图片搜索 MCP 服务

---
+++ len-image-search-mcp-server/src/test/java/com/lenyan/lenimagesearchmcpserver/LenImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lenyan.lenimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenImageSearchMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ len-image-search-mcp-server/src/test/java/com/lenyan/lenimagesearchmcpserver/tools/ImageSearchToolTest.java
@@ -0,0 +1,20 @@
+package com.lenyan.lenimagesearchmcpserver.tools;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ImageSearchToolTest {
+
+    @Resource
+    private ImageSearchTool imageSearchTool;
+
+    @Test
+    void searchImage() {
+        String result = imageSearchTool.searchImage("computer");
+        System.out.println(result);
+    }
+
+}
